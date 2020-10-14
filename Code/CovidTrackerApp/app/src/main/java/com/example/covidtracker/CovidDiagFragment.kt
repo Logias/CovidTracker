f7401cbf9da0abe1ec83cfb86a71be40bb918367
scysci <60407705+scysci@users.noreply.github.com>
2020-10-14 10:39:23 -0700

    Fixed the covid diag frag, need to make a results frag to receive data now

--- Code/CovidTrackerApp/app/src/main/java/com/example/covidtracker/CovidDiagFragment.kt
+++ Code/CovidTrackerApp/app/src/main/java/com/example/covidtracker/CovidDiagFragment.kt
@@ -1,9 +1,13 @@
 package com.example.covidtracker
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
@@ -21,24 +25,43 @@ class CovidDiagFragment : Fragment() {
         covidDiagViewModel = ViewModelProvider(this).get(CovidDiagViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_covid_diag, container, false)
 
-        val chipGroup1 = root.findViewById<TextView>(R.id.chipGroup)
+        val chipGroup1 = root.findViewById<ChipGroup>(R.id.chipGroup)
+        val chipGroup2 = root.findViewById<ChipGroup>(R.id.chipGroup2)
+        val analyzeButton = root.findViewById<Button>(R.id.analyzeButton)
+        val ageEntry = root.findViewById<EditText>(R.id.ageEntry)
 
-/**
-        chipGroup.setOnCheckedChangeListener { chipGroup, i ->
-            val chip: Chip = chipGroup.findViewById(i)
-            if (chip != null) Toast.makeText(
-                getApplicationContext(),
-                "Chip is " + chip.chipText,
-                Toast.LENGTH_SHORT
-            ).show()
+
+        chipGroup2.setOnCheckedChangeListener { group, checkedId: Int ->
+            val chip: Chip? = root.findViewById(checkedId)
+            chip?.let {
+                // Show the checked chip text on toast message
+                Log.e(TAG, chip.text as String)
+                // Do code here to pass Chip data to analyze frag using FragmentManager
+            }
         }
 
-        val chip: Chip = findViewById(R.id.chip)
-        chip.setOnCloseIconClickListener {
-            Toast.makeText(getApplicationContext(), "Close is Clicked", Toast.LENGTH_SHORT)
-                .show()
-        }
-*/
+
+        //this button should pass all the data including the age into next frag
+        analyzeButton.setOnClickListener(View.OnClickListener {
+            var msg = "Chips checked are:"
+            val chipsCount = chipGroup1.childCount
+            if (chipsCount == 0) {
+                msg += " None!!"
+            } else {
+                var i = 0
+                while (i < chipsCount) {
+                    val chip = chipGroup1.getChildAt(i) as Chip
+                    if (chip.isChecked) {
+                        msg += chip.text.toString() + " "
+                    }
+                    i++
+                }
+            }
+            // show message
+            Log.e(TAG, msg)
+        })
+
+
         return root
     }
 }
